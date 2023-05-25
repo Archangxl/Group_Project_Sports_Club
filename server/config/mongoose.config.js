3ca8a1b87304aca5ed43f2351edf84d868a1d919
Alexander Mueller <101312895+Archangxl@users.noreply.github.com>
2023-05-24 20:47:26 -0600

    Update mongoose.config.js

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const databaseName = "Project_Database";
-mongoose.connect(`mongodb://localhost/${databaseName}`, {
+mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
